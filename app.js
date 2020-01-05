33caa69174815bb81336a0a0cfd4ca36f7ee0475
stephen pierce <pierces@gmail.com>
2020-01-05 14:26:20 -0500

    client side is mostly done

--- app.js
+++ app.js
@@ -16,30 +16,37 @@ var server = http.createServer( function onRequest(req,res){
 io = io.listen(server);
 server.listen(8006);
 console.log("start");  
+
+function sendCurrentlyPlaying(socket){
+  var str = fs.readFileSync( 'pb.html', 'utf8');
+  if( str == "" ){
+    return;
+  }
+  //console.log( str );
+  socket.emit( 'date',
+    {date: str}
+  )
+}
+
 io.sockets.on('connection', function (socket) {
   console.log("connected");
 
   //read stationlist and send it to client.
+  var stationList = fs.readFileSync('pblist.html', 'utf8');
+  if( stationList != ""){
+    socket.emit('stations', {content:stationList});
+  }
+  sendCurrentlyPlaying(socket);
+  fs.watch( 'pb.html', sendCurrentlyPlaying(socket) );
 
-
-
-  fs.watch( '/var/www/html/pianobar/pb.html',
-  function( ev, file){
-        var str = fs.readFileSync( '/var/www/html/pianobar/pb.html', 'utf8');
-        if( str == "" ){
-          return;
-        }
-        //console.log( str );
-        socket.emit( 'date',
-          {date: str}
-        )
-      }
-    );
-  socket.on("play",function (socket){
+  socket.on("play",function (data){
     console.log("play");
   });
-  socket.on("next",function (socket){
+  socket.on("next",function (data){
     console.log("next");
   });
+  socket.on("changeStation", function( data){
+    console.log("changeStation", data.station);
+  })
 });
 
